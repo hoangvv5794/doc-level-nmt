479409f6b17add44d5c870f3886d529c80e5c4f3
hoangvv5794 <vuhoang5794@gmail.com>
2024-02-20 22:41:27 +0700

    push semantic segment

--- exp_gtrans/prepare-semantic.py
+++ exp_gtrans/prepare-semantic.py
@@ -21,53 +21,63 @@ def convert_to_segment(args):
         src_raw = read_file(path.join(args.datadir, dataset, source_lang_file))
         tgt_raw = read_file(path.join(args.datadir, dataset, target_lang_file))
 
-        src_doc = src_raw.split('<d>')
-        tgt_doc = tgt_raw.split('<d>')
+        src_doc_split = src_raw.split('<d>')
+        tgt_doc_split = tgt_raw.split('<d>')
+        for idx, (src_doc, tgt_doc) in enumerate(zip(src_doc_split, tgt_doc_split)):
+            # process for each source_doc and target_doc
+            # split document to each sentence
+            src_lines_split = src_doc.split('\r\n')
+            tgt_lines_split = tgt_doc.split('\r\n')
+            # push each sentence to BERT Model and vectorize each sentence
 
-        print(src_doc)
-        print(tgt_doc)
+            # compare and merge sentence to groups
 
-        # split by document
+            # split each group with delimiter <s>
 
-        # convert
-        # processed = []
-        # src_data = []
-        # tgt_data = []
-        # for idx, (src, tgt) in enumerate(zip(src_lines, tgt_lines)):
-        #     # check min doc length for training
-        #     if corpus == 'train' and len(src.split()) < args.min_train_doclen:
-        #         logger.warning('Skip too short document: corpus=train, doc=%s, sents=%s, tokens=%s'
-        #                        % (idx, len(src.split('</s>')), len(src.split())))
-        #         continue
-        #     # segment the doc
-        #     srcs, tgts = seg_func(src, tgt, args.max_sents)
-        #     # verify doc length
-        #     srcs_len = [len(line.split()) for line in srcs]
-        #     if any(l > args.max_tokens for l in srcs_len):
-        #         logger.warning('Source doc has too long segment: corpus=%s, doc=%s, sents=%s, seg_len=%s, max_len=%s.'
-        #                        % (corpus, idx, len(src.split('</s>')), max(srcs_len), args.max_tokens))
-        #     tgts_len = [len(line.split()) for line in tgts]
-        #     if any(l > args.max_tokens for l in tgts_len):
-        #         logger.warning('Target doc has too long segment: corpus=%s, doc=%s, sents=%s, seg_len=%s, max_len=%s.'
-        #                        % (corpus, idx, len(tgt.split('</s>')), max(tgts_len), args.max_tokens))
-        #     # persist
-        #     src_data.extend(srcs)
-        #     tgt_data.extend(tgts)
-        #     processed.append(idx)
-        # # remove special token
-        # if args.no_special_tok:
-        #     src_data = [line.replace('<s> ', '').replace(' </s>', '') for line in src_data]
-        #     tgt_data = [line.replace('<s> ', '').replace(' </s>', '') for line in tgt_data]
-        # # save segmented language files
-        # logger.info('Processed %s documents of %s with a max_len of %s.' % (len(processed), corpus, args.max_tokens))
-        # source_lang_file = '%s.%s' % (corpus, args.source_lang)
-        # target_lang_file = '%s.%s' % (corpus, args.target_lang)
-        # source_lang_file = path.join(args.destdir, source_lang_file)
-        # save_lines(source_lang_file, src_data)
-        # logger.info('Saved %s lines into %s' % (len(src_data), source_lang_file))
-        # target_lang_file = path.join(args.destdir, target_lang_file)
-        # save_lines(target_lang_file, tgt_data)
-        # logger.info('Saved %s lines into %s' % (len(tgt_data), target_lang_file))
+            # write out to file
+
+
+    # split by document
+
+    # convert
+    # processed = []
+    # src_data = []
+    # tgt_data = []
+    # for idx, (src, tgt) in enumerate(zip(src_lines, tgt_lines)):
+    #     # check min doc length for training
+    #     if corpus == 'train' and len(src.split()) < args.min_train_doclen:
+    #         logger.warning('Skip too short document: corpus=train, doc=%s, sents=%s, tokens=%s'
+    #                        % (idx, len(src.split('</s>')), len(src.split())))
+    #         continue
+    #     # segment the doc
+    #     srcs, tgts = seg_func(src, tgt, args.max_sents)
+    #     # verify doc length
+    #     srcs_len = [len(line.split()) for line in srcs]
+    #     if any(l > args.max_tokens for l in srcs_len):
+    #         logger.warning('Source doc has too long segment: corpus=%s, doc=%s, sents=%s, seg_len=%s, max_len=%s.'
+    #                        % (corpus, idx, len(src.split('</s>')), max(srcs_len), args.max_tokens))
+    #     tgts_len = [len(line.split()) for line in tgts]
+    #     if any(l > args.max_tokens for l in tgts_len):
+    #         logger.warning('Target doc has too long segment: corpus=%s, doc=%s, sents=%s, seg_len=%s, max_len=%s.'
+    #                        % (corpus, idx, len(tgt.split('</s>')), max(tgts_len), args.max_tokens))
+    #     # persist
+    #     src_data.extend(srcs)
+    #     tgt_data.extend(tgts)
+    #     processed.append(idx)
+    # # remove special token
+    # if args.no_special_tok:
+    #     src_data = [line.replace('<s> ', '').replace(' </s>', '') for line in src_data]
+    #     tgt_data = [line.replace('<s> ', '').replace(' </s>', '') for line in tgt_data]
+    # # save segmented language files
+    # logger.info('Processed %s documents of %s with a max_len of %s.' % (len(processed), corpus, args.max_tokens))
+    # source_lang_file = '%s.%s' % (corpus, args.source_lang)
+    # target_lang_file = '%s.%s' % (corpus, args.target_lang)
+    # source_lang_file = path.join(args.destdir, source_lang_file)
+    # save_lines(source_lang_file, src_data)
+    # logger.info('Saved %s lines into %s' % (len(src_data), source_lang_file))
+    # target_lang_file = path.join(args.destdir, target_lang_file)
+    # save_lines(target_lang_file, tgt_data)
+    # logger.info('Saved %s lines into %s' % (len(tgt_data), target_lang_file))
 
 
 if __name__ == '__main__':
